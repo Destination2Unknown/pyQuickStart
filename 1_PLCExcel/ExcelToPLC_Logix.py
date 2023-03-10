801d01bfb7bd5a0128cece6024d24110fd70f2cb
Destination2Unknown <92536730+Destination2Unknown@users.noreply.github.com>
2023-03-10 13:25:28 +0000

    Create ExcelToPLC_Logix.py

---
+++ 1_PLCExcel/ExcelToPLC_Logix.py
@@ -0,0 +1,31 @@
+import openpyxl
+from pylogix import PLC
+
+# Open the workbook
+wb = openpyxl.load_workbook('tagsLogix.xlsx')
+
+# Select the sheet
+sheet = wb['Sheet1']
+
+# Extract the tags from the A column, removing the column header
+tag_names = [cell.value for cell in sheet['A'] if cell.value][1:]
+# Extract the values from the B column, removing the column header
+tag_values = [cell.value for cell in sheet['B'] if cell.value][1:]
+
+# Connect to the PLC
+plc = PLC()
+plc.IPAddress = '192.168.123.100'
+plc.ProcessorSlot = 2
+plc.SocketTimeout = 2.5
+
+# Combine Tag Name with Tag Values
+tagsWithValue=list(zip(tag_names,tag_values))
+
+###### Option 1: Individual Write
+values = [plc.Write(t,v) for t,v in tagsWithValue]
+
+###### Option 2: Multi-Write
+values = plc.Write(tagsWithValue)
+
+# Disconnect from the PLC
+plc.Close()
